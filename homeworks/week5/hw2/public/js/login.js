a0d7d3d5b3ed31ad368db1aa2eec92c056e530c7
Amelie Cheng <amelie1815@gmail.com>
2018-10-13 21:27:01 +0800

    W5 hw2 scripts.ejs

--- homeworks/week5/hw2/public/js/login.js
+++ homeworks/week5/hw2/public/js/login.js
@@ -1,22 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('.login__btn').addEventListener('click', () => {
+document.querySelector('.login__btn').addEventListener('click', () => {
 
-		const username = document.querySelector('[name=username]');
-		const password = document.querySelector('[name=password]');
-    
-    const xhr = new XMLHttpRequest();
+  const username = document.querySelector('[name=username]');
+  const password = document.querySelector('[name=password]');
+  
+  const xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
 
-        if (xhr.responseText === 'error') showWarning('Invalid username or password');
-        if (xhr.responseText === 'ok') document.location.href = '/';
-      }
+      if (xhr.responseText === 'error') showWarning('Invalid username or password');
+      if (xhr.responseText === 'ok') document.location.href = '/';
     }
-    xhr.open('POST', '/login');
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(`username=${username.value}&password=${password.value}`);
-	})
+  }
+  xhr.open('POST', '/login');
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(`username=${username.value}&password=${password.value}`);
 })
 
 function showWarning(warning) {
--- homeworks/week5/hw2/public/js/register.js
+++ homeworks/week5/hw2/public/js/register.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('.register__btn').addEventListener('click', () => {
+document.querySelector('.register__btn').addEventListener('click', () => {
 
-		const username = document.querySelector('[name=username]');
-		const password = document.querySelector('[name=password]');
-    const nickname = document.querySelector('[name=nickname]');
-    
-    const xhr = new XMLHttpRequest();
+  const username = document.querySelector('[name=username]');
+  const password = document.querySelector('[name=password]');
+  const nickname = document.querySelector('[name=nickname]');
+  
+  const xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
 
-        if( xhr.responseText === 'error' ) showWarning('Username has already been taken');
-        if( xhr.responseText === 'ok' ) document.location.href = '/';
-      }
+      if( xhr.responseText === 'error' ) showWarning('Username has already been taken');
+      if( xhr.responseText === 'ok' ) document.location.href = '/';
     }
-    xhr.open('POST', '/register');
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(`username=${username.value}&password=${password.value}&nickname=${nickname.value}`);
-	})
+  }
+  xhr.open('POST', '/register');
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(`username=${username.value}&password=${password.value}&nickname=${nickname.value}`);
 })
 
 function showWarning(warning) {
