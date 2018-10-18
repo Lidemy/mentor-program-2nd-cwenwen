932b6677aa4553f7d14503bca3a563e9d1fbe6a9
Amelie Cheng <amelie1815@gmail.com>
2018-10-18 16:20:01 +0800

    W5 hw2 handle pages

--- homeworks/week5/hw2/controller/comment.js
+++ homeworks/week5/hw2/controller/comment.js
@@ -34,14 +34,62 @@ module.exports = {
   },
   
   showComments: (req, res) => {
-    sequelize.query('SELECT `comment`.`id` AS `commentId` , `comment`.`username` AS `username` , `comment`.`parentId` AS `parentId` , `comment`.`topic` AS `topic` , `comment`.`content` AS `content` , `comment`.`createdAt` AS `createdAt` , `user`.`nickname` AS `nickname` FROM `cwenwen_comments` AS `comment` INNER JOIN `cwenwen_users` AS `user` ON `comment`.`username` = `user`.`username` WHERE `comment`.`parentId` = 0 ORDER BY `comment`.`createdAt` DESC LIMIT 10;', { type: sequelize.QueryTypes.SELECT })
-      .then(comments => {
-        res.locals.comments = comments;
-        console.log(comments, typeof(comments[0].createdAt))
-        const nickname = req.session.nickname;
-        res.render('index', {nickname});
+    let totalPages,
+        currentPage,
+        commentStartNumber;
+
+    // check if user has logged in
+    if (req.session.username) {
+      res.locals.user = { 
+        username: req.session.username,
+        nickname: req.session.nickname
+      };
+    } else {
+      res.locals.user = {
+        username: undefined,
+        nickname: undefined
+      };
+    }
+    
+    // get the number of main comments
+    Comment
+      .findAll({
+        attributes: [[sequelize.fn('COUNT', sequelize.col('parentId')), 'datanum']],
+        where: {
+          parentId: 0
+        }
       })
-      .catch(error => {throw error})
+      .then(data => {
+        // calculate the number of pages
+        totalPages = Math.ceil(data[0].dataValues.datanum / 10); 
+        res.locals.totalPages = totalPages;
+
+        // handle invalid page number
+        if (parseInt(req.params.page) < 0 || parseInt(req.params.page) > totalPages || isNaN(parseInt(req.params.page))) {
+          res.redirect('/pages/1');
+
+        } else {
+          // set current page
+          currentPage = parseInt(req.params.page);
+          res.locals.currentPage = currentPage;
+
+          // find the first comment of each page
+          commentStartNumber = ( currentPage - 1 ) * 10;
+        }
+
+        // get 10 comments for current page
+        sequelize
+          .query('SELECT `comment`.`id` AS `commentId` , `comment`.`username` AS `username` , `comment`.`parentId` AS `parentId` , `comment`.`topic` AS `topic` , `comment`.`content` AS `content` , `comment`.`createdAt` AS `createdAt` , `user`.`nickname` AS `nickname` FROM `cwenwen_comments` AS `comment` INNER JOIN `cwenwen_users` AS `user` ON `comment`.`username` = `user`.`username` WHERE `comment`.`parentId` = 0 ORDER BY `comment`.`createdAt` DESC LIMIT ' + commentStartNumber + ', 10;', { type: sequelize.QueryTypes.SELECT })
+          .then(comments => {
+            res.locals.comments = comments;
+
+            const nickname = req.session.nickname;
+            res.render('index', {nickname});
+          })
+          .catch(error => {throw error})
+
+      })
+      .catch(error => { throw error })
   },
 
   modifyComment: (req, res) => {
